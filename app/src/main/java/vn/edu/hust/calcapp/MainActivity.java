365b14868259b474c2b7bbe72d3396792c86f3fa
quanganh.tran <quanganh.tran@192.168.1.51>
2019-10-20 03:12:13 +0700

    sua them

--- app/src/main/java/vn/edu/hust/calcapp/MainActivity.java
+++ app/src/main/java/vn/edu/hust/calcapp/MainActivity.java
@@ -13,9 +13,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     int op1, op2;   // gia tri 2 toan hang
+    double double1, double2;
     int op;         // 1: add, 2: sub, 3: mul, 4: div
     int state;      // 1: nhap op1, 2: nhap op2
-
+    boolean hasDecimal = false;
+    boolean error = false;
+    int temp1;
+    double temp2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Khoi tao cac gia tri
         op1 = 0;
         op2 = 0;
+        double1 = 0;
+        double2 = 0;
         state = 1;
         op = 0;
 
@@ -108,6 +114,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_rev:
                 reverseOperand();
                 break;
+            case R.id.btn_dot:
+                addDecimal();
+                break;
             case R.id.btn_bs:
                 removeDigit();
                 break;
@@ -122,13 +131,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void  addDecimal() {
+                //op1 = 0;
+        String tmp = tvResult.getText()+"";
+        if (tmp.contains(".")) tvResult.setText(tmp+"");
+        else tvResult.setText(tmp+".");
+//        if (state == 1) {
+//            double1 = Double.parseDouble(tmp)
+//        } else  {
+//
+//        }
+
+    }
+
     private void clearOperator() {
         // Khoi tao cac gia tri
         op1 = 0;
         op2 = 0;
+        double1 = 0;
+        double2 = 0;
+        temp1 = 0;
+        temp2 = 0;
         state = 1;
         op = 0;
-
+        error = false;
         tvResult.setText(String.valueOf(op1));
         opDisplay.setText(String.valueOf(op1));
     }
@@ -136,21 +162,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void clearOperand() {
         if (state == 1) {
             op1 = 0;
+            double1 = 0;
             tvResult.setText(String.valueOf(op1));
         } else {
             op2 = 0;
+            double2 = 0;
             tvResult.setText(String.valueOf(op2));
-
         }
     }
 
     private void removeDigit() {
+        String tmp = tvResult.getText()+"";
         if (state == 1) {
             op1 = op1 / 10;
-            tvResult.setText(String.valueOf(op1));
+            if (tmp.contains(".")) {
+                String s = tmp.substring(0, tmp.length() - 1);
+                double1 = Double.parseDouble(s);
+                tvResult.setText(s);
+            }
+            else tvResult.setText(String.valueOf(op1));
         } else {
-            op2 = op2 / 10;
-            tvResult.setText(String.valueOf(op2));
+            if (tmp.contains(".")) {
+                String s = tmp.substring(0, tmp.length() - 1);
+                double2 = Double.parseDouble(s);
+                tvResult.setText(s);
+            }
+            else tvResult.setText(String.valueOf(op2));
         }
     }
 
@@ -165,49 +202,99 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void calcResult() {
-
         int result = 0;
-        switch (op) {
-            case 1: {
-                opDisplay.setText(String.valueOf(op1) +"+"+ String.valueOf(op2));
-                result = op1 + op2; break;
-            }
-            case 2: {
-                opDisplay.setText(String.valueOf(op1) +"-"+ String.valueOf(op2));
-                result = op1 - op2; break;
-            }
-            case 3: {
-                opDisplay.setText(String.valueOf(op1) +"*"+ String.valueOf(op2));
-                result = op1 * op2; break;
-            }
-            case 4: {
-                opDisplay.setText(String.valueOf(op1) +"/"+ String.valueOf(op2));
-                result = op1 / op2; break;
+        double result2 = 0;
+        String tmp = opDisplay.getText()+"";
+        String tmp2 = tvResult.getText()+"";
+        String ops = " +-*/";
+        if (tmp2.contains(".")) {
+            switch (op) {
+                case 1: result2 = double1 + double2; break;
+                case 2: result2 = double1 - double2; break;
+                case 3: result2 = double1 * double2; break;
+                case 4: {
+                    if (double2 != 0) {result2 = double1 / double2; }
+                    else error = true;
+                    break;
+                }
+                case 0: result2 = Double.parseDouble(tmp2);break;
             }
         }
-        tvResult.setText(String.valueOf(result));
+        else {
+            switch (op) {
+                case 1: result = op1 + op2; break;
+                case 2: result = op1 - op2; break;
+                case 3: result = op1 * op2; break;
+                case 4: {
+                    if (op2 != 0) {result = op1 / op2; }
+                    else error = true;
+                    break;
+                }
+                case 0: result = Integer.parseInt(tmp2);break;
+            }
+        }
+
+        temp1 = result;
+        temp2 = result2;
+        if (op==0) {
+            if (tmp2.contains(".")) opDisplay.setText(String.valueOf(result2));
+            else opDisplay.setText(String.valueOf(result));
+        }
+        else {
+            if (tmp2.contains(".")) opDisplay.setText(Double.toString(double1) +ops.charAt(op)+Double.toString(double2));
+            else  opDisplay.setText(String.valueOf(op1)+ops.charAt(op)+String.valueOf(op2));
+        }
+        if (tmp2.contains(".")) tvResult.setText(error ? "ERROR" :String.valueOf(result2));
+        else tvResult.setText(error ? "ERROR" :String.valueOf(result));
+
         state = 1;
-        op1 = result;
-        op2 = 0;
+        op1 = op2 = 0;
+        double1 = double2 = 0;
         op = 0;
     }
 
     private void selectOperator(int p) {
+        String tmp = opDisplay.getText()+"";
+        if (tmp.contains("+") || tmp.contains("-") || tmp.contains("*") || tmp.contains("/")) {op1 = temp1;double1 = temp2;}
         op = p;
         state = 2;
-
+        String ops = " +-*/";
         op2 = 0;
-        tvResult.setText(String.valueOf(op2));
-        opDisplay.setText(String.valueOf(op1));
+        double2 = 0;
+        String tmp2 = tvResult.getText()+"";
+        if (tmp2.contains(".")) {
+            double1 = Double.parseDouble(tmp2);
+            tvResult.setText(Double.toString(double2));
+            opDisplay.setText(tmp+ops.charAt(op));
+        }else {
+            tvResult.setText(String.valueOf(op2));
+            opDisplay.setText(tmp+ops.charAt(op));
+        }
+
     }
 
     private void addValue(int c) {
         if (state == 1) {
-            op1 = op1 * 10 + c;
-            tvResult.setText(String.valueOf(op1));
+
+            String tmp = tvResult.getText()+String.valueOf(c);
+            if (tmp.contains(".")) {
+                double1 = Double.parseDouble(tmp);
+                tvResult.setText(String.valueOf(double1));
+            } else {
+                op1 = Integer.parseInt(tmp);
+                tvResult.setText(String.valueOf(op1));
+            }
         } else {
-            op2 = op2 * 10 + c;
-            tvResult.setText(String.valueOf(op2));
+            String tmp = tvResult.getText()+String.valueOf(c);
+            if (tmp.contains(".")) {
+                double2 = Double.parseDouble(tmp);
+                tvResult.setText(String.valueOf(double2));
+            } else {
+                op2 = Integer.parseInt(tmp);
+                tvResult.setText(String.valueOf(op2));
+            }
+
         }
+
     }
 }
